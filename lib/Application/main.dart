19ee7f442ade278e8b6115b66714a203e8a4569f
Alerika <alessiaiaconantonio.ai@gmail.com>
2022-11-03 20:40:40 +0100

    pretty code
    Miglioramento codice

--- lib/Application/main.dart
+++ lib/Application/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:greenpark/Application/App.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:greenpark/controllers/login_controller.dart';
+import 'package:greenpark/pages/WelcomePage.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  await Firebase.initializeApp();
-  runApp(const App());
+
+  runApp(App());
+}
+@override
+Widget App() {
+  return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => LoginController(),
+          child: WelcomePage(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Start application',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const WelcomePage(),
+      ));
 }
